4edbad9d6f7d6a822cdb508099c073a6c39563fa
Harshit Santosh <harshitsantosh@example.com>
2020-12-03 13:36:15 +0530

    added Bootstrap to the Main Page

--- prj_django_webcam/wsgi.py
+++ prj_django_webcam/wsgi.py
@@ -14,5 +14,5 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prj_django_webcam.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/path/to/static/files')
-application.add_files('/path/to/more/static/files', prefix='more-files/')
+# application = WhiteNoise(application, root='/path/to/static/files')
+# application.add_files('/path/to/more/static/files', prefix='more-files/')
